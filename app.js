2a9a5803df85650505b1fb182397f050ab9e416e
Gabriel Brickle <gabebrickle@Gabriels-MacBook-Pro.local>
2016-06-08 15:45:55 -0400

    still working on completed button

--- app.js
+++ app.js
@@ -39,27 +39,48 @@ var gabeToDo = {
     $(this).parent().remove();
     gabeToDo.deleteToDo(toDoId);
   });
+  ////editing a post
+  var newLiVal;
+  $("ul").on('dblclick', 'li', function () {
+    newLiVal = $(this).text();
+    $(this).text("");
+    $("<input type='text' data-id='"+ $(this).data('id') +"'>").appendTo(this).focus();
+  });
+
+
+  $("ul").on('focusout', 'li > input', function () {
+    var $this = $(this);
+    var newLiVal = $this.val()
+    $this.text($this.val());
+    $(this).parent().text($this.val()).append('<a href=""> ✓</a>');
+    // console.log($(this).data('id'))
+    $this.remove();
+    gabeToDo.editToDo({
+      todo: newLiVal,
+      _id: $this.data('id')
+    })
+
+
+  });
+
+
   ///Clicking on buttons
    $('button').on('click',function(event) {
     event.preventDefault();
     console.log(this);
   });
-    //   var thingWeClickText = $(this).text();
-    //   var ourClassToShow = "." + thingWeClickText.toLowerCase();
-    //
-    // if(thingWeClickText.toLowerCase() === 'all') {
-    //   cconsole.log(thingWeClickText.text);
-    //   // $(ourClassToShow).removeClass('all')
-    //   // $(ourClassToShow).siblings().addClass('hidden')
-    // } else {
-    //   var htmlStr = gabeToDo.htmlGenerator(blogTemplates[thingWeClickText.toLowerCase()])
-    //   $(ourClassToShow).removeClass('hidden').append(htmlStr);
-    //   $(ourClassToShow).siblings().addClass('hidden')
-    // }
-    // }),
+    /////clicking on active button$("#toolbar li").each(function(){
     $('.clear').on('click',function(){
       event.preventDefault();
       $('ul').toggle('li');
+    });
+    // ///counting list items for tasks remaining
+
+    // var listItems = $("li").children();
+
+    $('.activebutton').on('click', function(){
+      event.preventDefault();
+      $.each($('li'),function(idx,element) { $(element).has('a').css("display",'block') });
     });
 
     $('.allbutton').on('click',function(){
@@ -78,6 +99,7 @@ var gabeToDo = {
       success: function(data) {
         console.log("works", data);
           gabeToDo.todos.push(data);
+          gabeToDo.getToDo();
       },
       error: function(err) {
         console.error("OH CRAP", err);
@@ -91,6 +113,7 @@ var gabeToDo = {
       method: "GET",
       success: function(data) {
         console.log("WE GOT SOMETHING", data);
+        $(".tasksremaining").find('h5').text("Tasks Remaining: " + data.length ); ///this counts the list items
         $('ul').html("");
         data.forEach(function(element,idx) {
           var toDoStr = `<li data-id="${element._id}">${element.todo}<a href=""> ✓</a></li>`
@@ -111,36 +134,34 @@ var gabeToDo = {
         method: "DELETE",
         success: function(data) {
           console.log("WE DELETED SOMETHING", data);
+          gabeToDo.getToDo();
+
         },
         error: function(err) {
           console.error("ugh", err);
         }
       })
-  },
+    },
+
+    editToDo: function(thingToEdit) {
+      console.log("THING TO EDIT", thingToEdit);
+      $.ajax({
+        url: gabeToDo.url + "/" + thingToEdit._id,
+        method: "PUT",
+        data: thingToEdit,
+        success: function(data) {
+          console.log("edited!!!!!",data)
+          gabeToDo.getToDo();
+        },
+        error: function(err) {
+          console.error("OH CRAP", err);
+        }
+      })
 
 
-        // ///clicking on buttons
-        //    $('.actions').on('click','button'function(event) {
-        //     event.preventDefault();
-        //     var thingWeClickText = $(this).text();
-        //     var ourClassToShow = "." + thingWeClickText.toLowerCase();
-        //
-        //   if(thingWeClickText.toLowerCase() === 'all') {
-        //     cconsole.log(thingWeClickText.text);
-        //     // $(ourClassToShow).removeClass('all')
-        //     // $(ourClassToShow).siblings().addClass('hidden')
-        //   } else {
-        //     var htmlStr = gabeToDo.htmlGenerator(blogTemplates[thingWeClickText.toLowerCase()])
-        //     $(ourClassToShow).removeClass('hidden').append(htmlStr);
-        //     $(ourClassToShow).siblings().addClass('hidden')
-        //   }
-        //   }),
-        // ///deleting a to do item i.e. checking the box
-        //   $('.toDoPost').on('click','button', function(event) {
-        //     event.preventDefault();
-        //     var blogId = $(this).parent().data('id');
-        //     gabeToDo.deleteToDo(????);
-        //   })
+  }, ///ending
+
+
 
 
 //
